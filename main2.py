9ecae6e51dfcf0a3d9b1b9bdfbc887c26d401a86
stanleymw <107821509+stanleymw@users.noreply.github.com>
2025-10-03 17:46:16 -0400

    finish function

--- main2.py
+++ main2.py
@@ -24,12 +24,18 @@ a = x + y + z
 print(a)
 """
 
+source4 = """
+y = 56
+z = y + 1
+print(y)
+print(z)
+"""
 source3 = """
 def add(a,b):
-    return a+b, b-a
+    return (a * 2) + b
 
-print(1+2)
-x = 69
+y = 7
+print(add(y,y))
 """
 
 tree = ast.parse(source3)
@@ -45,24 +51,84 @@ class IntValue(QueueInstruction):
 
 class FuncValue(QueueInstruction):
     value: str = ""
-    def __init__(self, val):
+    num_args: int = 0
+    num_returns: int = 0
+    def __init__(self, val, num_args, num_returns):
         self.value = val
+        self.num_args = num_args
+        self.num_returns = num_returns
 
-class ClacVisitor(ast.NodeVisitor):
-    variables: dict[str, QueueInstruction] = {
-        "print": FuncValue("1 pick print")
-    }
-    current_stack_position = 0
-    generated = ""
+def get_return_amount(ret: ast.Return):
+    match ret.value.__class__:
+        case ast.Tuple:
+            return len(ret.value.elts)
+        case ast.Name | ast.Constant | ast.BinOp:
+            return 1
+        case _:
+            raise Exception("Unknown return type")
+
+class ClacCompile(ast.NodeVisitor):
+    def __init__(self):
+        self.variables: dict[str, QueueInstruction] = {
+            "print": FuncValue("print", 1, 0)
+        }
+        self.current_stack_position = 0
+        self.generated = ""
+
+    def compile(self, node):
+        print(self,"variables:", self.variables)
+        self.visit(node)
+        
+        return self.generated
 
     def visit_FunctionDef(self, node):
-        print(f"Found function: {node.name}")
+        print(f"Found function def: {node.name}")
         self.generated += f": {node.name} "
+
+        new_compile = ClacCompile()
+        args = node.args.args
+
+        self.variables[node.name] = FuncValue(node.name, len(args), get_return_amount(node.body[-1]))
+
+        # print("(0) parent vars:", self.variables, "child vars:", new_compile.variables)
+        for i in range(len(args)):
+            new_compile.variables[args[i].arg] = IntValue(i + 1)
+
+        new_compile.current_stack_position = len(args)
+
+        new_compile.variables[node.name] = self.variables[node.name]
+
+        # print("parent:", self, "child:", new_compile)
+        # print("(1) parent vars:", self.variables, "child vars:", new_compile.variables)
+        new_compile.generic_visit(node)
+        # print("(2) parent vars:", self.variables, "child vars:", new_compile.variables)
+
+        self.generated +=  new_compile.generated
+
+        print("{node.name} generated:", new_compile.generated, "\nret count:", new_compile.current_stack_position)
+
+
         # Important: Continue traversing child nodes
-        self.generic_visit(node)
         self.generated += " ; \n"
 
+    def visit_Return(self, node):
+        self.generic_visit(node)
+        # asd asd asd a b
+        if node.value.__class__ == ast.Tuple:
+            assert(len(node.value.elts) == 2)
+
+            while (self.current_stack_position > 2):
+                self.generated += " rot drop "
+                self.current_stack_position -= 1
+        else:
+            # should be one item
+            while (self.current_stack_position > 1):
+                self.generated += " swap drop "
+                self.current_stack_position -= 1
+        print("RETURNING", self, node.value)
+
     def visit_Assign(self, node):
+        # print("MY VARIABLES:", self.variables, self)
         assert(len(node.targets)==1)
         assert(node.targets[0].ctx.__class__ == ast.Store)
 
@@ -74,7 +140,7 @@ class ClacVisitor(ast.NodeVisitor):
     def visit_Name(self, node):
         match node.ctx.__class__:
           case ast.Load:
-              print(f"Loading {node.id}")
+              print(self,f"Loading {node.id}")
               val = self.variables[node.id]
               match val.__class__.__name__:
                   case "IntValue":
@@ -104,11 +170,17 @@ class ClacVisitor(ast.NodeVisitor):
         self.generic_visit(node)
 
     def visit_Call(self, node):
+        func = self.variables[node.func.id]
+
+        assert(len(node.args) == func.num_args)
         self.generic_visit(node)
 
         # FIXME: every defined function needs to report how much they change the stack position
         # REQUIREMENT FOR FUNCTIONS: THE STACK SIZE MUST NOT CHANGE
+
         self.generated += f" {self.variables[node.func.id].value} "
+
+        self.current_stack_position += func.num_returns - func.num_args
 
         # stack offset modify should be #RETURN_VALUES - #ARGUMENTS
 
@@ -125,13 +197,12 @@ class ClacVisitor(ast.NodeVisitor):
         opname = (node.op.__class__.__name__)
         assert(opname in binops.keys())
 
-        print(f"binop: {opname}")
+        print(f"BINOP {self}: {opname}")
         self.generic_visit(node)
         self.generated += f" {binops[opname]} "
         self.current_stack_position -= 1
 
-cv = ClacVisitor()
-cv.visit(tree)
-print(cv.generated)
+cv = ClacCompile()
+print(cv.compile(tree))
 print([(i,v.value) for (i,v) in cv.variables.items()])
 print("Stack pos:", cv.current_stack_position)
